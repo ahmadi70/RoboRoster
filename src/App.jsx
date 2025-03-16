7bcdcd8a1040855155bf200443d9cb85bc62be06
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-16 20:29:41 +0100

    import and use searchBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import CardList from './components/card-list/card-list.component'
+import SearchBox from './components/search-box/search-box.component'
 
 const App = () => {
   const [robos, setRobos] = useState([])
@@ -15,16 +16,17 @@ const App = () => {
       })
   }, [])
 
+  const onSearchChange = event => {
+    const searchString = event.target.value.toLocaleLowerCase()
+    const newFilteredRobos = robos.filter( robo =>
+      robo.name.toLowerCase().includes(searchString)
+    )
+    setFilteredRobos(newFilteredRobos)
+  }
+
   return (
     <>
-      <input type='search' onChange={ event => {
-        const searchString = event.target.value.toLocaleLowerCase()
-        const newFilteredRobos = robos.filter( robo =>
-          robo.name.toLowerCase().includes(searchString)
-        )
-        setFilteredRobos(newFilteredRobos)
-      }} />
-
+      <SearchBox onChangeHandler={onSearchChange} />
       <CardList robos={filteredRobos} />
     </>
   )
