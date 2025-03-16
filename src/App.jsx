6adf8802bea3218a548d112a7f959f00801625b0
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-16 20:20:02 +0100

    filtering robos by search input

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,28 @@ import CardList from './components/card-list/card-list.component'
 
 const App = () => {
   const [robos, setRobos] = useState([])
+  const [filteredRobos, setFilteredRobos] = useState([])
 
   useEffect( () => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then( response => response.json())
-      .then( data => setRobos(data))
+      .then( data => {
+        setRobos(data)
+        setFilteredRobos(data)
+      })
   }, [])
 
   return (
     <>
-      <CardList robos={robos} />
+      <input type='search' onChange={ event => {
+        const searchString = event.target.value.toLocaleLowerCase()
+        const newFilteredRobos = robos.filter( robo =>
+          robo.name.toLowerCase().includes(searchString)
+        )
+        setFilteredRobos(newFilteredRobos)
+      }} />
+
+      <CardList robos={filteredRobos} />
     </>
   )
 }
