bc54ecd74e430c2a2dc4ec3bec51a4197a08d6e3
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-16 20:03:48 +0100

    import and use card-list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from 'react'
 import './App.css'
+import CardList from './components/card-list/card-list.component'
 
-function App() {
+const App = () => {
+  const [robos, setRobos] = useState([])
+
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then( response => response.json())
+      .then( data => setRobos(data))
+  }, [])
 
   return (
     <>
-      Hello From React
+      <CardList robos={robos} />
     </>
   )
 }
